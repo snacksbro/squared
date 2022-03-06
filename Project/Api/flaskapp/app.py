f9e0d3e4910d56228973c52a644bd8d94ad89e04
JWLorenzo <jacob.w.lorenzo@maine.edu>
2022-03-05 23:07:46 -0500

    Added diceroll feature
    Added diceroll function to flask

--- Project/Api/flaskapp/app.py
+++ Project/Api/flaskapp/app.py
@@ -1,15 +1,21 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
+
+@app.route('/diceroll')
+def RandListd6():
+    return random.randint(1,6)
 
 @app.route('/')
 def index():
     return '<h1>Squared</h1>'
 
 
+
 @app.route('/login_user')
 def login_user():
     '''
-    fetch the params and process the request 
+    fetch the params and process the request
     validate the params using joi
     '''
