bef4d37624b934649bd70f9b4c3e25d9985a2503
snacksbro <tyler.j.walker@maine.edu>
2022-03-08 08:11:13 -0500

    Add gameCreate function

--- Project/Api/flaskapp/app.py
+++ Project/Api/flaskapp/app.py
@@ -8,7 +8,27 @@ game = [
 			"board": [["red", "blue", "none"],
 								["none", "blue", "blue"]],
 			"turn": "user1",
-			"roll": 3}]
+			"roll": 3}
+]
+runningGames = []
+
+def gameCreate(players): # from lobby we'll queue players
+	# later we'll let them choose their color
+	gameID = random.randInt(1, 99999)
+	# Generate the board
+	board = []
+	for x in range(10):
+		for y in range(10):
+			board[x][y] = "none"
+
+	game[gameID] = {
+			"players": ["test1", "test2"],
+			"playerColors": ["red", "blue"],
+			"board": board,
+			"turn": "test1",
+			"roll": 0
+	}
+	
 
 @app.route('/diceroll')
 def RandD6():
@@ -22,14 +42,12 @@ def RandListGen(RandomList, ItemCount):
         ListInteger = random.randint(0, len(RandomList)-1)
         ItemList.append(RandomList[ListInteger])
         RandomList.remove(RandomList[ListInteger])
-		ItemList.sort()
+    ItemList.sort()
     return ItemList
 
 @app.route('/')
 def index():
     return '<h1>Squared</h1>'
-
-
 
 @app.route('/login_user')
 def login_user():
@@ -37,3 +55,4 @@ def login_user():
     fetch the params and process the request
     validate the params using joi
     '''
+
