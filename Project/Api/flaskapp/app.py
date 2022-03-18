b0b81c36012ddf9061b3cd2b660cd6ddeb229fc9
Snacksbro <tyler.j.walker@maine.edu>
2022-03-17 20:16:47 -0400

    Add numerous functions for testing validity

--- Project/Api/flaskapp/app.py
+++ Project/Api/flaskapp/app.py
@@ -19,9 +19,10 @@ game = [
 # TODO: Add a /queue route. Players who join the game will go there, once there's 4, add them to a game
 runningGames = []
 
+# Yolo, I'll fix this later. I'm putting this on the outside so it's a global for testing purposes
+gameID = random.randint(1, 99999)
 def gameCreate(players): # from lobby we'll queue players
 	# later we'll let them choose their color
-	gameID = random.randInt(1, 99999)
 	# Generate the board
 	board = []
 	for x in range(10):
@@ -60,16 +61,45 @@ def StringParser(StringToBeParsed):
 	ParsedList = StringToBeParsed.split(",")
 	return ParsedList
 
+# isAdjacent: Returns true if squares are adjacent on the board, false otherwise
+def isAdjacent(coord1, coord2):
+	# Making a list of every adjacent square
+	possibilities = [
+		[coord1[0]+1, coord1[1]], # right
+		[coord1[0], coord1[1]+1], # down
+		[coord1[0]-1, coord1[1]], # left
+		[coord1[0], coord1[1]-1], # up
+	]
+	
+	# Checking if any of them "hit" coord2
+	for possiblitiy in possibilities:
+		# If so, return True
+		if possiblitiy[0] == coord2[0] and possiblitiy[1] == coord2[1]:
+			return True
+	# If not, return False
+	return False
+
+# translateSquare: Takes in a square ("a2" for example) and returns an index ([0, 2] for example)
+def translateSquare(square):
+	alphabet = "abcdefghijk"
+	return [alphabet.index(square[0]), int(square[1])]
+
 @app.route('/verify')
-def VerifyTile(TileString):
-	if "none" in StringParser(TileString):
-		return 0
-	elif "trap" in StringParser(TileString):
-		return 1
-	elif "red" in StringParser(TileString):
-		return 2
-	elif "blue" in StringParser(TileString):
-		return 3
+def VerifyTile():
+	# TODO: Add gameid to this
+	position = translateSquare(game[gameID].positions[game[gameID].players.index(game[gameID].turn)])
+	target = translateSquare(request.args.get("square"))
+	TileString = game[gameID].board[target[0]][target[1]]
+
+	if (isAdjacent(position, target)):
+		if "none" in StringParser(TileString):
+			return 0
+		elif "trap" in StringParser(TileString):
+			return 1
+		elif "red" in StringParser(TileString):
+			return 2
+		elif "blue" in StringParser(TileString):
+			return 3
 
 @app.route('/login_user')
 def login_user():
