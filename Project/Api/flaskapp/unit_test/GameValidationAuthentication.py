cdad63df770971cb873525d1cc32cc8d392e2ce9
JWLorenzo <jacob.w.lorenzo@maine.edu>
2022-05-01 23:01:39 -0400

    Update GameValidationAuthentication.py

--- Project/Api/flaskapp/unit_test/GameValidationAuthentication.py
+++ Project/Api/flaskapp/unit_test/GameValidationAuthentication.py
@@ -5,5 +5,5 @@ from controller.validation.gamevalidation import is_adjacent
 
 class TestAuthentication(unittest.TestCase):
 
-    def test_login_func(self):
+    def test_isadjacent_func(self):
         self.assertEqual(is_adjacent([1,1], [1,2]),True)
